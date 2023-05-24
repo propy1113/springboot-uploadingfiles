4c510857555c4ee3f3fdecb7ceeaf88e7b8610f4
propy1113 <50749341+propy1113@users.noreply.github.com>
2023-05-24 10:42:43 +0900

    Add files via upload

---
+++ springboot-uploadingfiles/src/main/java/com/example/uploadingfiles/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.uploadingfiles.storage;
+
+public class StorageFileNotFoundException<T> extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
